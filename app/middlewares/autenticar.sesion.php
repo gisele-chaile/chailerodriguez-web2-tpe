19e77c605d633b6affd383240cc01cca9c570144
gisele-chaile <giselechaile@gmail.com>
2024-10-21 00:00:58 -0300

    Update autenticar.sesion.php

--- app/middlewares/autenticar.sesion.php
+++ app/middlewares/autenticar.sesion.php
@@ -4,7 +4,7 @@
     if(isset($_SESSION['ID_usuario'])){
         $respuesta->usuario = new stdClass();
         $respuesta->usuario->ID_usuario = $_SESSION['ID_usuario'];
-        $respuesta->usuario->usuario = $_SESSION['ususario'];
+        $respuesta->usuario->usuario = $_SESSION['usuario'];
         return;
     }
    }
